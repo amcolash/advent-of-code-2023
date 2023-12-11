b71f0f7a97f517b9228c5a9d802e0dd8da5317c9
Andrew McOlash <amcolash@gmail.com>
2023-12-11 00:20:30 -0800

    day 9

---
+++ src/9/part1.ts
@@ -0,0 +1,38 @@
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+const data = readFileSync(join(__dirname, 'input.txt')).toString();
+const lines = data.trim().split('\n');
+
+const nextValues: number[] = [];
+lines.forEach((line, l) => {
+  const levels: number[][] = [];
+  levels[0] = line.split(' ').map((c) => Number.parseInt(c));
+
+  let i = 0;
+  while (!levels[i].every((v) => v === 0)) {
+    levels[i + 1] = [];
+
+    const cur = levels[i];
+    const next = levels[i + 1];
+
+    for (let j = 0; j < cur.length - 1; j++) {
+      const left = cur[j];
+      const right = cur[j + 1];
+      next.push(right - left);
+    }
+
+    i++;
+
+    // console.log(levels);
+  }
+
+  let sum = 0;
+  for (let i = levels.length - 1; i >= 0; i--) {
+    sum += levels[i][levels[i].length - 1];
+  }
+
+  nextValues.push(sum);
+});
+
+console.log(nextValues.reduce((a, b) => a + b, 0));
